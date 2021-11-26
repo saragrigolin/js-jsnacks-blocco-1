1001b1145e8f1083058ea4cf4f72fa372ad5aa5a
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-11-26 13:34:05 +0100

    add script to ask number

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.
+
+// let somma = 0;
+// for (let index = 0; index < 10; index++) {
+//     let numero = prompt('Inserisci un numero');
+//     somma += parseInt(numero);
+// }
+// console.log(somma);
+
+let somma = 0;
+let i = 0;
+while (i < 10) {
+    let numero = prompt('Inserisci un numero');
+    somma += parseInt(numero);
+    i++;
+}
+console.log(somma);
