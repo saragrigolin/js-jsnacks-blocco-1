d45dad647cbd6eefc101ac3c80aef2c320e0e3e9
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-11-26 13:46:45 +0100

    change folder

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,4 +1,5 @@
-// Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.
+/* Jsnack1
+Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.*/
 
 // let somma = 0;
 // for (let index = 0; index < 10; index++) {
